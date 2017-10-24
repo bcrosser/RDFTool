882986f8299b9a00cc14044762e3af37f98c595a
Brendan <Brendan@VERIFEX>
2017-10-23 20:30:23 -0700

    changes

---
+++ RDFTool/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VDS.RDF;
+
+namespace RDFTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://github.com/dotnetrdf/dotnetrdf/wiki/UserGuide-Reading-RDF
+            IGraph g = new Graph();
+            using (var client = new System.Net.WebClient())
+            {
+                var rdfText = client.DownloadString("http://localhost/w/index.php/Special:ExportRDF/Walmart");
+                g.LoadFromString(rdfText);
+            }
+            Console.WriteLine($"Hi there, I've got {g.Triples.Count} triples");
+            //TurtleParser tp = new TurtleParser();
+            //tp.Load(g, )
+        }
+    }
+}
